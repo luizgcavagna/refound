49c262d5b9b0c02a38d28292adc0699886d84f90
Luiz G Cavagna <luizcavagna@gmail.com>
2026-04-30 16:11:00 -0300

    finish

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
+const expenseList = document.querySelector("ul");
+const expenseQuantity = document.querySelector("aside header p span");
+const expensesTotal = document.querySelector("aside header h2")
+
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, '');
 
@@ -32,5 +36,110 @@ form.onsubmit = (event) => {
     created_at: new Date()
   }
 
-  console.log(newExpense)
+  expenseAdd(newExpense);
+}
+
+function expenseAdd(newExpense) {
+  try {
+
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `./img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense
+
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    expenseInfo.append(expenseName, expenseCategory);
+
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace("R$", "")}`;
+
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "./img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
+
+    expenseList.append(expenseItem);
+
+    updateTotals();
+    formClear();
+
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.");
+    console.log(error);
+  }
+}
+
+function updateTotals() {
+  try {
+    
+    const items = expenseList.children;
+
+    expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "depesas" : "despesa"}`
+
+    let total = 0;
+
+    for(let item = 0; item < items.length; item++){
+      const itemAmount = items[item].querySelector(".expense-amount");
+
+      let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".");
+      
+      value = parseFloat(value);
+
+      if(isNaN(value)){
+        return alert(
+          "Não foi possível calcular o total. O valor não parece ser um número."
+        );
+      }
+
+      total += Number(value);
+    }
+
+
+    const symbolBRL = document.createElement("small");
+    symbolBRL.textContent = "R$";
+
+    total = formatCurrencyBRL(total).toUpperCase().replace("R$", "");
+
+    expensesTotal.innerHTML = '';
+
+    expensesTotal.append(symbolBRL, total);
+
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível atualizar os totais.");
+  }
+}
+
+expenseList.addEventListener("click", function(event) {
+  if(event.target.classList.contains("remove-icon")) {
+    const item = event.target.closest(".expense");
+
+    item.remove();
+
+    updateTotals();
+  }
+});
+
+function formClear() {
+  expense.value = "";
+  category.value = "";
+  amount.value = "";
+
+  expense.focus();
+
 }
